1bcde5f936c912a2382db6c919053824dcd9cce6
benjaego <benjaego@gmail.com>
2016-05-19 22:25:28 +0800

    EMDD version1.0
    EMDD version1.0

--- EMDD.py
+++ EMDD.py
@@ -1,6 +1,5 @@
-
 import numpy as np
-import scipy.optimize as optimize
+from scipy import optimize
 import random
 from sklearn import cross_validation
 import time
@@ -12,56 +11,84 @@ _floatX = np.float32
 _intX = np.int8
 
 
-class MaxDiverseDensity(object):
-    """
-    bags is a list of bag
-    each bag is a dict required following <key, value>
-    key: inst_prob, value: a vector indicating each instance's probability
-    key: label, value: a scalar indicating this bag's label
-    key: prob, value: a scalar indicating this bag's probability
-    key: instances, value: a numpy array indicating instances in this bag, each row is a instance, each colum is a
-    feature
-    """
+class EMDiverseDensity(object):
 
     def __init__(self):
         pass
 
-    def diverse_density_nll(self, params, bags):
-
-        fun = 0
-        n_bag = len(bags)
-        n_dim = bags[0]['instances'].shape[1]
-        # parameter length equal to feature length, only learn target vector
+    def diverse_density_nll(self, params, instances, labels):
+        [n_instances, n_dim] = instances.shape
         if params.shape[0] == n_dim:
             target = params
             scale = np.ones(n_dim, )
-        # parameter length equal to 2*feature length, learn target vector and scaling vector both
         else:
             target = params[0:n_dim]
             scale = params[n_dim:]
 
-        # compute negative log likelihood
-        for bag_idx in range(n_bag):
-            instances = bags[bag_idx]['instances']
-            dist = np.mean(((instances - target) ** 2) * (scale ** 2), axis=1)
-            bags[bag_idx]['inst_prob'] = np.exp(-dist)
-            bags[bag_idx]['prob'] = 1 - np.prod(1 - np.asarray(bags[bag_idx]['inst_prob']))
+        fun = 0
 
-            if bags[bag_idx]['label'] == 1:
-                if bags[bag_idx]['prob'] == 0:
-                    bags[bag_idx]['prob'] = 1e-10
-                fun = fun - np.log(bags[bag_idx]['prob'])
+        dist = np.mean(((instances - target) ** 2) * (scale ** 2), axis=1)
+        inst_prob = np.exp(-dist)
+        for inst_idx in range(n_instances):
+            if labels[inst_idx] == 1:
+                if inst_prob[inst_idx] == 0:
+                    inst_prob[inst_idx] = 1e-10
+                fun -= np.log(inst_prob[inst_idx])
             else:
-                if bags[bag_idx]['prob'] == 1:
-                    bags[bag_idx]['prob'] = 1 - 1e-10
-                fun = fun - np.log(1 - bags[bag_idx]['prob'])
+                if inst_prob[inst_idx] == 1:
+                    inst_prob[inst_idx] = 1 - 1e-10
+                fun -= np.log(1 - inst_prob[inst_idx])
         return fun
 
-    def train(self, bags, scale_indicator, epochs):
+    def em(self, bags, func_val_tol, scale_indicator, init_target, init_scale):
+        target = init_target
+        scale = init_scale
 
+        # select an optimal instance from each bag according to current target and scale
+        func_val_diff = np.inf
+        prev_func_val = np.inf
+        final_func_val = 0
+        init_func_val = 0
+        while func_val_diff > func_val_tol:
+            selected_instances = list()
+            selected_labels = list()
+            # select an instance with highest probability from each bag
+            for bag in bags:
+                instances = np.asarray(bag['instances'])
+                [_, n_dim] = instances.shape
+                dist = np.mean(((instances - target) ** 2) * (scale ** 2), axis=1)
+                bag['inst_prob'] = np.exp(-dist)
+                max_idx = np.argmax(bag['inst_prob'])
+                selected_instances.append(instances[max_idx, :])
+                selected_labels.append(bag['label'])
+
+            selected_instances = np.asarray(selected_instances)
+
+            if scale_indicator == 1:
+                init_params = np.hstack((target, scale))
+                init_func_val = self.diverse_density_nll(init_params, selected_instances, selected_labels)
+                r_params = optimize.minimize(self.diverse_density_nll, init_params,
+                                             args=(selected_instances, selected_labels,), method='L-BFGS-B')
+                target = r_params.x[0:n_dim]
+                scale = r_params.x[n_dim:]
+            else:
+                init_params = target
+                init_func_val = self.diverse_density_nll(init_params, selected_instances, selected_labels)
+                r_params = optimize.minimize(self.diverse_density_nll, init_params,
+                                             args=(selected_instances, selected_labels,), method='L-BFGS-B')
+                target = r_params.x
+                scale = np.ones(n_dim,)
+
+            final_func_val = r_params.fun
+            func_val_diff = prev_func_val - final_func_val
+            prev_func_val = final_func_val
+
+        return target, scale, final_func_val, init_func_val
+
+    def train(self, bags, scale_indicator, epochs, func_val_tol=1e-5):
         n_bag = len(bags)
-
         n_pos_bag = 0
+
         max_iter = 0
         for bag in bags:
             if bag['label'] == 1:
@@ -77,43 +104,34 @@ class MaxDiverseDensity(object):
         func_values = list()
 
         for epoch_idx in range(epochs):
+            # randomly select a positive bag
             bag_idx = random.randint(0, n_bag - 1)
             while bags[bag_idx]['label'] == 0 or np.all(np.asarray(bags[bag_idx]['starting_point']) == 1):
                 bag_idx = random.randint(0, n_bag - 1)
-
+            # randomly select a positive instance that not used before
             [_, n_dim] = bags[bag_idx]['instances'].shape
             starting_point_bag = np.asarray(bags[bag_idx]['starting_point'])
             valuable_idx = np.asarray(np.nonzero(starting_point_bag == 0))
             if valuable_idx.shape[1] == 1:
                 instance_idx = valuable_idx[0, 0]
             else:
-                rand_idx = random.randint(0, valuable_idx.shape[1]-1)
+                rand_idx = random.randint(0, valuable_idx.shape[1] - 1)
                 instance_idx = valuable_idx[0, rand_idx]
             bags[bag_idx]['starting_point'][instance_idx] = 1
 
-            if scale_indicator == 1:
-                init_params = np.hstack((bags[bag_idx]['instances'][instance_idx, :], np.ones([n_dim, ])))
-                r_params = optimize.minimize(self.diverse_density_nll, init_params, args=(bags,), method='L-BFGS-B')
-                print('epoch %d, selected instance is from <bag %d, bag label %d, instance %d>. '
-                      'nll before optimization is %f, nll after optimization is %f' %
-                      (epoch_idx, bag_idx, bags[bag_idx]['label'], instance_idx,
-                       self.diverse_density_nll(init_params, bags),
-                       self.diverse_density_nll(r_params.x, bags)))
-                targets.append(r_params.x[:n_dim])
-                scales.append(r_params.x[n_dim:])
-                func_values.append(r_params.fun)
+            # scale is initialized to one
+            print('epoch %d, selected instance is from <bag %d, bag label %d, instance %d>. ' %
+                  (epoch_idx, bag_idx, bags[bag_idx]['label'], instance_idx), end='')
+            [target, scale, func_val, init_func_val] = self.em(bags, func_val_tol,
+                                                               scale_indicator,
+                                                               bags[bag_idx]['instances'][instance_idx, :],
+                                                               np.ones(n_dim, ))
+            print('nll before optimization is %f, nll after optimization is %f' % (init_func_val, func_val))
 
-            else:
-                init_params = bags[bag_idx]['instances'][instance_idx, :]
-                r_params = optimize.minimize(self.diverse_density_nll, init_params, args=(bags,), method='L-BFGS-B')
-                print('epoch %d, selected instance is from <bag %d, bag label %d, instance %d>. '
-                      'nll before optimization is %f, nll after optimization is %f' %
-                      (epoch_idx, bag_idx, bags[bag_idx]['label'], instance_idx,
-                       self.diverse_density_nll(init_params, bags),
-                       self.diverse_density_nll(r_params.x, bags)))
-                targets.append(r_params.x)
-                scales.append(np.ones(n_dim,))
-                func_values.append(r_params.fun)
+            targets.append(target)
+            scales.append(scale)
+            func_values.append(func_val)
+
         return targets, scales, func_values
 
     def predict(self, targets, scales, func_values, bags, aggregate, threshold):
@@ -158,100 +176,9 @@ class MaxDiverseDensity(object):
         return bags_label, bags_prob, instances_label, instances_prob
 
 
-def toy_example():
-
-    dd_classifier = MaxDiverseDensity()
-
-    train_bags = list()
-    train_labels = list()
-
-    n_dim = random.randint(5, 10)
-    n_train_bags = random.randint(20, 100)
-
-    n_positive_bag = 0
-    n_negative_bag = 0
-    idx = 0
-
-    while idx < n_train_bags:
-        bag = dict()
-        if random.random() < 0.5:
-            mu = 10
-            sig = 1.0
-            bag['label'] = 1
-            train_labels.append(1)
-            bag['prob'] = 0
-            n_instances = np.random.randint(1, 20)
-            bag['inst_prob'] = np.random.random([n_instances, ])
-            bag['starting_point'] = np.zeros([n_instances, ])
-            bag['instances'] = np.random.normal(mu, sig, [n_instances, n_dim])
-            train_bags.append(bag)
-            idx += 1
-            n_positive_bag += 1
-        else:
-            mu = 0.0
-            sig = 1.0
-            bag['label'] = 0
-            train_labels.append(0)
-            bag['prob'] = 0
-            n_instances = np.random.randint(1, 20)
-            bag['inst_prob'] = np.random.random([n_instances, ])
-            bag['starting_point'] = np.zeros([n_instances, ])
-            bag['instances'] = np.random.normal(mu, sig, [n_instances, n_dim])
-            train_bags.append(bag)
-            idx += 1
-            n_negative_bag += 1
-
-    print('positive bags number is:', n_positive_bag)
-    print('negative bags number is: ', n_negative_bag)
-
-    idx = 0
-    n_test_bags = random.randint(10, 30)
-    test_bags = list()
-    test_labels = list()
-
-    while idx < n_test_bags:
-        bag = dict()
-        if random.random() > 0.5:
-            mu = 10
-            sig = 1.0
-            bag['label'] = 1
-            test_labels.append(1)
-            bag['prob'] = 0
-            n_instances = np.random.randint(1, 20)
-            bag['inst_prob'] = np.random.random([n_instances, ])
-            bag['starting_point'] = np.zeros([n_instances, ])
-            bag['instances'] = np.random.normal(mu, sig, [n_instances, n_dim])
-            test_bags.append(bag)
-            idx += 1
-        else:
-            mu = 0.0
-            sig = 1.0
-            bag['label'] = 0
-            test_labels.append(0)
-            bag['prob'] = 0
-            n_instances = np.random.randint(1, 20)
-            bag['inst_prob'] = np.random.random([n_instances, ])
-            bag['starting_point'] = np.zeros([n_instances, ])
-            bag['instances'] = np.random.normal(mu, sig, [n_instances, n_dim])
-            test_bags.append(bag)
-            idx += 1
-
-    print('test bags number is:', n_test_bags)
-
-    targets, scales, fvals = dd_classifier.train(train_bags, scale_indicator=1, epochs=1)
-
-    aggregate = 'max'
-    threshold = 0.5
-
-    p_bags_label, p_bags_prob, p_instances_label, p_instances_prob = dd_classifier.predict(targets, scales,
-                                                                                           fvals, test_bags,
-                                                                                           aggregate, threshold)
-    print('testing accuracy is: %f' % (sum(test_labels == p_bags_label)/n_test_bags))
-
-
-def maxDD_musk1(split_ratio=None, cv_fold=None, aggregate='avg', threshold=0.5, scale_indicator=1, epochs=10):
+def EMDD_musk1(split_ratio=None, cv_fold=None, aggregate='avg', threshold=0.5, scale_indicator=1, epochs=10):
     start_time = time.clock()
-    dd_classifier = MaxDiverseDensity()
+    dd_classifier = EMDiverseDensity()
     file_path = 'musk1.txt'
     bags, bag_labels = load_musk1_data(file_path)
     if split_ratio is None and cv_fold is None:
@@ -266,7 +193,7 @@ def maxDD_musk1(split_ratio=None, cv_fold=None, aggregate='avg', threshold=0.5, 
         predict_result = (p_bags_label, p_bags_prob, p_instances_label, p_instances_prob)
         data = (bags, bag_labels)
         end_time = time.clock()
-        print('time elapsed in maxDD is %f seconds' % (end_time - start_time))
+        print('time elapsed in EMDD is %f seconds' % (end_time - start_time))
         return data, train_result, predict_result
 
     elif split_ratio:
@@ -286,7 +213,7 @@ def maxDD_musk1(split_ratio=None, cv_fold=None, aggregate='avg', threshold=0.5, 
         predict_result = (p_bags_label, p_bags_prob, p_instances_label, p_instances_prob)
         data = (bags, bag_labels)
         end_time = time.clock()
-        print('time elapsed in maxDD is %f seconds' % (end_time - start_time))
+        print('time elapsed in EMDD is %f seconds' % (end_time - start_time))
         return data, train_result, predict_result
     elif cv_fold:
         accuracy_list = list()
@@ -319,7 +246,7 @@ def maxDD_musk1(split_ratio=None, cv_fold=None, aggregate='avg', threshold=0.5, 
         mean_accuracy = float(np.mean(np.asarray(accuracy_list)))
         print('mean accuracy with %d-fold cross validation is %f' % (cv_fold, mean_accuracy))
         end_time = time.clock()
-        print('time elapsed in maxDD is %f seconds' % (end_time - start_time))
+        print('time elapsed in EMDD is %f seconds' % (end_time - start_time))
 
         return accuracy_list
     else:
@@ -327,5 +254,4 @@ def maxDD_musk1(split_ratio=None, cv_fold=None, aggregate='avg', threshold=0.5, 
 
 
 if __name__ == '__main__':
-    # toy_example()
-    maxDD_musk1(split_ratio=None, cv_fold=10, aggregate='min', threshold=0.5, scale_indicator=1, epochs=20)
+    EMDD_musk1(split_ratio=None, cv_fold=10, aggregate='min', threshold=0.5, scale_indicator=1, epochs=20)
